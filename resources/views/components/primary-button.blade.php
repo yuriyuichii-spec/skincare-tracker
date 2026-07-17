964a4db36ef90801c5207fc28e8aa4cd3d3a15b2
yuriyuichii-spec <yuriyuichii@gmail.com>
2026-07-17 18:15:59 +0800

    Update primary-button.blade.php

--- resources/views/components/primary-button.blade.php
+++ resources/views/components/primary-button.blade.php
@@ -1,3 +1,3 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150']) }}>
+<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-5 py-2 bg-blush-400 border border-transparent rounded-full font-semibold text-sm text-white hover:bg-blush-500 focus:bg-blush-500 active:bg-blush-600 focus:outline-none focus:ring-2 focus:ring-blush-300 focus:ring-offset-2 transition ease-in-out duration-150']) }}>
     {{ $slot }}
 </button>
