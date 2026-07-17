767a18227a4e2d678aeafd543c16575ecfe00e2d
yuriyuichii-spec <yuriyuichii@gmail.com>
2026-07-17 18:16:36 +0800

    Update text-input.blade.php

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,3 +1,3 @@
 @props(['disabled' => false])
 
-<input @disabled($disabled) {{ $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm']) }}>
+<input @disabled($disabled) {{ $attributes->merge(['class' => 'border-pink-200 focus:border-blush-400 focus:ring-blush-300 rounded-xl shadow-sm']) }}>
